04f3d9e3768f1c8f8b8d03a07218df6ea6af9dc0
jjaemun <jjaemuuu@gmail.com>
2026-03-08 16:58:39 +0100

    constants

--- calibration/constants.py
+++ calibration/constants.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-
+# type: ignore
 
 import torch
 
 
+'''  integral!  '''
 u16 = torch.uint16
 u32 = torch.uint32
 u64 = torch.uint64
@@ -12,10 +13,19 @@ i16 = torch.int16
 i32 = torch.int32
 i64 = torch.int64
 
-f16 = torch.float16
+
+'''  fp!  '''
 f32 = torch.float32
 f64 = torch.float64
 
-c32 = torch.complex32
+
+'''  complex!  '''
 c64 = torch.complex64
 c128 = torch.complex128
+
+
+'''  constants!  '''
+EPS: f64 = torch.tensor(1e-8, dtype = f64)
+PI: f64 = torch.acos(torch.tensor(-1.0, dtype = f64))
+J: c128 = torch.tensor(1j, dtype = c128)
+
