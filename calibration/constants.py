0349b9fa996c38a0328f4415bcfa27672968f5fb
jjaemun <jjaemuuu@gmail.com>
2026-03-08 23:33:54 +0100

    constants

--- calibration/constants.py
+++ calibration/constants.py
@@ -8,11 +8,11 @@ import torch
 
 
 '''  constants!  '''
-ZERO: f64 = torch.tensor(0.0, dtype = f64)
-ONE: f64 = torch.tensor(1.0, dtype = f64)
-PI: f64 = torch.acos(torch.tensor(-1.0, dtype = f64))
-UB: f64 = torch.tensor(1e3, dtype = f64)
-EPS: f64 = torch.tensor(1e-8, dtype = f64)
-J: c128 = torch.tensor(1j, dtype = c128)
+ZERO: Tensor[f64]  = torch.tensor(0.0, dtype = f64)
+EPS:  Tensor[f64]  = torch.tensor(1e-8, dtype = f64)
+ONE:  Tensor[f64]  = torch.tensor(1.0, dtype = f64)
+J:    Tensor[c128] = torch.tensor(1j, dtype = c128)
+PI:   Tensor[f64]  = torch.acos(torch.tensor(-1.0, dtype = f64))
+UB:   Tensor[f64]  = torch.tensor(1e3, dtype = f64)
 
 
