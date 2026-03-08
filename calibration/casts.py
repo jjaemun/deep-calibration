bee21ad9e19daf6bff06a354bb5626d133503345
jjaemun <jjaemuuu@gmail.com>
2026-03-08 16:58:21 +0100

    casts

---
+++ calibration/casts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import typing
+
+
+def cast(*tensors, to: typing.Type) -> typing.Tuple:
+
+    ''' Casts all input tensors to a different type. '''
+    
+    return tuple(
+        tensor.to(to) for tensor in tensors
+    )
