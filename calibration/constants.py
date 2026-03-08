4d268d8357c0ed1f65c29501212d86c8c7f2bef3
jjaemun <jjaemuuu@gmail.com>
2026-03-08 23:29:20 +0100

    uncivilized

--- calibration/constants.py
+++ calibration/constants.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
 # type: ignore
 
+
+from .hints import *
+
 import torch
-
-
-'''  integral!  '''
-u16 = torch.uint16
-u32 = torch.uint32
-u64 = torch.uint64
-
-i16 = torch.int16
-i32 = torch.int32
-i64 = torch.int64
-
-
-'''  fp!  '''
-f32 = torch.float32
-f64 = torch.float64
-
-
-'''  complex!  '''
-c64 = torch.complex64
-c128 = torch.complex128
 
 
 '''  constants!  '''
