e236ecbef224adca2bb97aa4874a2c7b332d9dbc
jjaemun <jjaemuuu@gmail.com>
2026-03-08 23:29:33 +0100

    hints

---
+++ calibration/hints.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# type: ignore
+
+import torch
+import typing
+
+# tensor!
+class Tensor(object):
+
+    ''' Type hinting helper. '''
+    
+    def __class_getitem__(cls, dtype: torch.dtype) -> typing.Type:
+        
+        ''' Type hinting clearly does not enforce behavior but
+                helps reason about code. '''
+
+        return typing.Annotated[torch.Tensor, dtype]
+
+
+# types!
+u16 = torch.uint16
+u32 = torch.uint32
+u64 = torch.uint64
+
+i16 = torch.int16
+i32 = torch.int32
+i64 = torch.int64
+
+f32 = torch.float32
+f64 = torch.float64
+
+c64 = torch.complex64
+c128 = torch.complex128
+
+
