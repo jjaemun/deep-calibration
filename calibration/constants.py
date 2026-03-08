5c410f154472f31a369e7a791c4616ac78f107a2
jjaemun <jjaemuuu@gmail.com>
2026-03-08 22:40:11 +0100

    updates;

--- calibration/constants.py
+++ calibration/constants.py
@@ -25,7 +25,11 @@ c128 = torch.complex128
 
 
 '''  constants!  '''
-EPS: f64 = torch.tensor(1e-8, dtype = f64)
+ZERO: f64 = torch.tensor(0.0, dtype = f64)
+ONE: f64 = torch.tensor(1.0, dtype = f64)
 PI: f64 = torch.acos(torch.tensor(-1.0, dtype = f64))
+UB: f64 = torch.tensor(1e3, dtype = f64)
+EPS: f64 = torch.tensor(1e-8, dtype = f64)
 J: c128 = torch.tensor(1j, dtype = c128)
+
 
