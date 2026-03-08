d2280d8b7fe4d29e1b90967c7b1f0aadaaa1a69b
jjaemun <jjaemuuu@gmail.com>
2026-03-08 16:20:48 +0100

    constants

---
+++ calibration/constants.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+import torch
+
+
+u16 = torch.uint16
+u32 = torch.uint32
+u64 = torch.uint64
+
+i16 = torch.int16
+i32 = torch.int32
+i64 = torch.int64
+
+f16 = torch.float16
+f32 = torch.float32
+f64 = torch.float64
+
+c32 = torch.complex32
+c64 = torch.complex64
+c128 = torch.complex128
